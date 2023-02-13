0578f2bc26a254ac2cd093897af41ee2ffb97859
Charles Swaleh <charlswall@live.com>
2023-02-13 23:01:40 +0300

    Created the navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,44 @@
 import React from "react";
 
 function App() {
-
 	return (
 		<div>
-			<h1>React setup successful</h1>
+			<nav class="navbar navbar-expand-lg">
+				<div class="container-fluid nav-container">
+					<a class="navbar-brand" href="/">
+						Canopy.CAFFE
+					</a>
+					<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" aria-current="page" href="/">
+									Home
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/menu">
+									Menu
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/cart">
+									Cart
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/about">
+									About Us
+								</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/contacts">
+									Contacts
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 		</div>
 	);
 }
