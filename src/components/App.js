575b446ea9a1caba1040f615023f4dd66446e851
Charles Swaleh <charlswall@live.com>
2023-02-16 18:03:50 +0300

    Moved the HomePage to its own component

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Homepage from "./Homepage"
 
 function App() {
 	return (
 		<div>
 			<NavBar />
-			<div id="header">
-				<h1 className="hero-heading">DRINKS TO JACK YOUR MIND</h1>
-				<p className="tagline">Darkest Berries. Sweetest...</p>
-				<p className="tag-inventory">We stock the sickest (no pun intended) inventory of Coffee, Tea, Juices, Milkshakes, Pastries and more. <br/> Whatever it takes to get you moving.</p>
-				<div className="home-buttons">
-					<button className="btn btn-custom" type="button" name="menu-button">
-						EXPLORE
-					</button>
-					<button className="btn btn-custom" type="button" name="menu-button">
-						LEARN MORE
-					</button>
-				</div>
-			</div>
+			<Homepage />
 		</div>
 	);
 }
---
+++ src/components/Homepage.js
@@ -0,0 +1,19 @@
+export default function Homepaage() {
+	return (
+		<div id="header">
+			<h1 className="hero-heading">DRINKS TO JACK YOUR MIND</h1>
+			<p className="tagline">Darkest Berries. Sweetest...</p>
+			<p className="tag-inventory">
+				We stock the sickest (no pun intended) inventory of Coffee, Tea, Juices, Milkshakes, Pastries and more. <br /> Whatever it takes to get you moving.
+			</p>
+			<div className="home-buttons">
+				<button className="btn btn-custom" type="button" name="menu-button">
+					EXPLORE
+				</button>
+				<button className="btn btn-custom" type="button" name="menu-button">
+					LEARN MORE
+				</button>
+			</div>
+		</div>
+	);
+}
