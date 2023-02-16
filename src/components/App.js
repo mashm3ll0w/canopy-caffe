bd6c99405c2b9817a340a0622fbb98e59f717ccd
Charles Swaleh <charlswall@live.com>
2023-02-16 23:29:40 +0300

    Created the CafeMenu component that will render all the CafeItem(s) components

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import {Routes, Route} from "react-router-dom"
 import Homepage from "./Homepage"
 import About from "./About";
 import Contact from "./Contact";
+import CafeMenu from "./CafeMenu";
 
 function App() {
 	return (
 		<div>
       <Routes>
         <Route path="/" element={<Homepage />}/>
+        <Route path="/menu" element={<CafeMenu />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/contacts" element={<Contact />}/>
       </Routes>
---
+++ src/components/CafeMenu.js
@@ -0,0 +1,7 @@
+export default function CafeMenu() {
+	return (
+		<div className="menu-container">
+    {/* this is where you'll render all the menu items */}
+		</div>
+	);
+}
