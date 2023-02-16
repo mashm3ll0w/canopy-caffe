b38aea1783102343f87b8924306d48deb74709a4
Charles Swaleh <charlswall@live.com>
2023-02-16 18:32:58 +0300

    Create the About component

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+export default function About() {
+	return (
+		<div id="about">
+			<h1 className="hero-about">WHO ARE WE?</h1>
+			<p>We are the most highly acclaimed Caffe in Nairobi.</p>
+      <p>Don't take our word for it. Come see for yourself.</p>
+		</div>
+	);
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom"
 import Homepage from "./Homepage"
+import About from "./About";
 
 function App() {
 	return (
 		<div>
       <Routes>
         <Route path="/" element={<Homepage />}/>
+        <Route path="/about" element={<About />}/>
       </Routes>
 		</div>
 	);
