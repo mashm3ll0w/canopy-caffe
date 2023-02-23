32d7430da73789c6f06b934bf00b4e099a2791b1
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-23 22:23:44 +0300

    Creating FormData

--- src/components/NewCafeItem.js
+++ src/components/NewCafeItem.js
@@ -1,7 +1,14 @@
 import React,{ useState } from "react";    
 
 function NewCafeItem() { 
-const [formData, setFormData] = useState({});
+const [formData, setFormData] = useState({
+    name: "", 
+    type: "",
+    price:"",
+    size:"",
+    description:"",
+    poster_url: "",   
+  });  
 
 
 return (
