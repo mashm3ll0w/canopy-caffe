14b33f186ede0aca38a84e1be2818769b9241ff3
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-24 15:00:13 +0300

    Completed NewCafeItem Component Design

--- src/components/NewCafeItem.js
+++ src/components/NewCafeItem.js
@@ -62,21 +62,31 @@ function NewCafeItem({ onAddToMenu}) {
   }
 
 return (
+  <>
+
 		<div className="form-container container-fluid" style={formStyle}>
-			<form className="row gy-2 gx-3 align-items-center">
+
+      <section>
+       <h2>READY FOR A NEW GRUB ADVENTURE?</h2>
+        <span><label htmlFor = "name"> GrandPa Rick wants you to wistfully conceive the next Interdimensional Menu Hit for Canopy Caffe to Suit Your Fancy!</label></span>
+        <span><label htmlFor = "name"> Don't be wicked Morty, You got this! </label></span>
+      </section>
+
+      <form className="row gy-2 gx-3 align-items-left">
 				<div className="mb-3">
 					<label htmlFor="menuName" className="form-label">
 						Menu Name
 					</label>
-					<input type="text" id="menuName" className="form-control" name="name" placeholder="" onChange={handleChange} value={formData.name} />
+					<input type="text" id="menuName" className="form-control" name="name" placeholder="Enter Name" onChange={handleChange} value={formData.name} />
 				</div>
 
 				<div className="mb-3">
 					<label htmlFor="menuType" className="form-label">
 						Menu Type
 					</label>
-					<select id="menuType" name="type" className="form-select" onChange={handleChange} value={formData.type}>
-						<option value="Coffee">Coffee</option>
+					<select id="menuType" name="type" className="form-select" defaultValue={"default"} onChange={handleChange} value={formData.type}>
+            <option value={"default"}> Choose an option </option>
+            <option value="Coffee">Coffee</option>
 						<option value="Tea">Tea</option>
 						<option value="Juice">Juice</option>
 						<option value="Milkshake">Milkshake</option>
@@ -88,8 +98,9 @@ return (
 					<label htmlFor="menuSize" className="form-label">
 						Menu Size
 					</label>
-					<select id="menuSize" name="size" className="form-select" onChange={handleChange} value={formData.size}>
-						<option value="large">Large</option>
+					<select id="menuSize" name="size" className="form-select" defaultValue={"default"}  onChange={handleChange} value={formData.size}>
+            <option value={"default"}> Choose an option </option>
+            <option value="large">Large</option>
 						<option value="medium">Medium</option>
 						<option value="small">Small</option>
 					</select>
@@ -99,21 +110,21 @@ return (
 					<label htmlFor="menuPrice" className="form-label">
 						Price in KES
 					</label>
-					<input id="menuPrice" type="number" className="form-control" name="price" placeholder="" onChange={handleChange} value={formData.price} />
+					<input id="menuPrice" type="number" className="form-control" name="price" placeholder="Enter Price" onChange={handleChange} value={formData.price} />
 				</div>
 
 				<div className="mb-3">
 					<label htmlFor="menuDescription" className="form-label">
 						Description
 					</label>
-					<input id="menuDescription" type="text" className="form-control" name="description" placeholder="" onChange={handleChange} value={formData.description} />
+					<input id="menuDescription" type="text" className="form-control" name="description" placeholder="Enter Item Description" onChange={handleChange} value={formData.description} />
 				</div>
 
 				<div className="mb-3">
 					<label htmlFor="menuImage" className="form-label">
 						Image
 					</label>
-					<input id="menuImage" type="text" className="form-control" name="poster_url" placeholder="" onChange={handleChange} value={formData.poster_url} />
+					<input id="menuImage" type="text" className="form-control" name="poster_url" placeholder="Dunk on A Morty Character" onChange={handleChange} value={formData.poster_url} />
 				</div>
 
 				<div className="col-auto">
@@ -125,6 +136,7 @@ return (
 				</div>
 			</form>
 		</div>
+    </>
 	);
 }
 
