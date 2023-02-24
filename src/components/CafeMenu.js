31fb197302f02b1f89e0f083479a6ec73b6e8176
Charles Swaleh <charlswall@live.com>
2023-02-24 19:41:01 +0300

    Styled the filter component

--- src/components/CafeMenu.js
+++ src/components/CafeMenu.js
@@ -2,31 +2,35 @@ import CafeItem from "./CafeItem";
 import Filter from "./Filter";
 import { useNavigate } from "react-router-dom";
 
+const searchStyle = {
+	display: "flex",
+	// padding: "20px",
+	justifyContent: "center",
+	// gap: "50px",
+};
+
 export default function CafeMenu({ menu, handleSearch, filter }) {
 	const navigate = useNavigate();
-  const afterFilter = menu.filter((item)=>item.name.toLowerCase().includes(filter.toLocaleLowerCase()))
-  
-	function handleClick(){
-		navigate("/menu/new")
+	const afterFilter = menu.filter((item) => item.name.toLowerCase().includes(filter.toLocaleLowerCase()));
+
+	function handleClick() {
+		navigate("/menu/new");
 	}
 
 	return (
 		<>
-     <Filter filter={filter} handleSearch={handleSearch} />
-		 <div class="col text-center">
-		   <button 
-		   className="btn btn-custom" 
-		   type="button" 
-		   name="menu-button" 
-		   onClick={handleClick}>CREATE NEW ITEM
-			</button>
-		</div>
+			<div class="col text-center" style={searchStyle}>
+				<button className="btn btn-custom" type="button" name="menu-button" onClick={handleClick}>
+					CREATE NEW ITEM
+				</button>
+				<Filter filter={filter} handleSearch={handleSearch} />
+			</div>
 
-		<div className="menu-container container-fluid">
+			<div className="menu-container container-fluid">
 				{afterFilter.map((item) => (
 					<CafeItem key={item.id} item={item} />
 				))}
-		</div>
+			</div>
 		</>
 	);
 }
