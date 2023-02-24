d71dd918236b1d8d6a9ceb51e939542c03a78b19
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-24 13:08:10 +0300

    New Route Path Deployed - NewCafeItem

--- src/components/CafeMenu.js
+++ src/components/CafeMenu.js
@@ -1,12 +1,25 @@
 import CafeItem from "./CafeItem";
+import { useNavigate } from "react-router-dom";
 
-export default function CafeMenu({ menu, onAddToMenu }) {
+export default function CafeMenu({ menu }) {
+	const navigate = useNavigate();
+
+	function handleClick(){
+		navigate("/menu/new")
+	}
+
 	return (
 		<>
-		   <button className="btn btn-custom" type="button" name="menu-button" onClick={handleClick}>EXPLORE</button>
+		   <button 
+		   className="btn btn-custom" 
+		   type="button" 
+		   name="menu-button" 
+		   onClick={handleClick}>CREATE NEW ITEM
+			</button>
+
 			<div className="menu-container container-fluid">
 				{menu.map((item) => (
-					<CafeItem key={item.id} item={item} />
+				<CafeItem key={item.id} item={item} />
 				))}
 			</div>
 		</>
